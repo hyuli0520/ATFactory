4e35b0626041a4b29eab7c889b05a821912f50db
hyuli0520 <minhyeogjeon945@gmail.com>
2025-09-18 09:48:31 +0900

    feat: Update Player Controller
    Space to Jump

--- Assets/3.Scripts/Object/Player/PlayerController.cs
+++ Assets/3.Scripts/Object/Player/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float mouseSensitivity = 1f;
     public float gravity = -9.81f;
+    public float jumpHeight = 2f;
 
     private IMovement _movement;
     private IInputReader _inputReader;
@@ -16,7 +17,7 @@ public class PlayerController : MonoBehaviour
         var input = new InputSystem_Actions();
 
         _inputReader = new NewInputReader(input);
-        _movement = new PlayerMovement(controller, moveSpeed, gravity);
+        _movement = new PlayerMovement(controller, moveSpeed, gravity, jumpHeight);
         _look = new FPSLook(Camera.main.transform, transform, mouseSensitivity);
     }
 
@@ -30,5 +31,8 @@ public class PlayerController : MonoBehaviour
 
         _movement.Move(moveDir);
         _look.Look(inputRotation);
+
+        if (_inputReader.ReadJump())
+            _movement.Jump();
     }
 }
