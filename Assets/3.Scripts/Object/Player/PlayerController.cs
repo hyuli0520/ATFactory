e0808c22f5e1f29999ff3a0f02a115ab939f902d
hyuli0520 <minhyeogjeon945@gmail.com>
2025-09-18 11:43:19 +0900

    feat: Create Player Mining

--- Assets/3.Scripts/Object/Player/PlayerController.cs
+++ Assets/3.Scripts/Object/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,10 +7,14 @@ public class PlayerController : MonoBehaviour
     public float mouseSensitivity = 1f;
     public float gravity = -9.81f;
     public float jumpHeight = 2f;
+    public float range = 3f;
 
     private IMovement _movement;
     private IInputReader _inputReader;
     private ILook _look;
+    private IMiningTool _tool;
+
+    private bool _isMining = false;
 
     private void Awake()
     {
@@ -19,6 +24,7 @@ public class PlayerController : MonoBehaviour
         _inputReader = new NewInputReader(input);
         _movement = new PlayerMovement(controller, moveSpeed, gravity, jumpHeight);
         _look = new FPSLook(Camera.main.transform, transform, mouseSensitivity);
+        _tool = new Pickaxe();
     }
 
     private void Update()
@@ -34,5 +40,27 @@ public class PlayerController : MonoBehaviour
 
         if (_inputReader.ReadJump())
             _movement.Jump();
+
+        if (_inputReader.ReadLeftClick() && !_isMining)
+            TryMine();
+    }
+
+    private void TryMine()
+    {
+        var camera = Camera.main;
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out RaycastHit hit, range))
+        {
+            var mineable = hit.collider.GetComponent<IMineable>();
+            if (mineable != null)
+                StartCoroutine(MineRoutine(mineable));
+        }
+    }
+
+    private IEnumerator MineRoutine(IMineable target)
+    {
+        _isMining = true;
+        yield return new WaitForSeconds(_tool.MiningTime);
+        _tool.Use(target);
+        _isMining = false;
     }
 }
