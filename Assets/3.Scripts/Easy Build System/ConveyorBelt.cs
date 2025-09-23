a7492bfd633d0396b78f5a43ab1992e94e86850f
hyuli0520 <minhyeogjeon945@gmail.com>
2025-09-23 18:05:32 +0900

    feat: Create ConveyorBelt

---
+++ Assets/3.Scripts/Easy Build System/ConveyorBelt.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public List<GameObject> onBelt;
+
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i <= onBelt.Count - 1; i++)
+        {
+            onBelt[i].GetComponent<Rigidbody>().linearVelocity = speed * direction * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onBelt.Add(collision.gameObject);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        onBelt.Remove(collision.gameObject);
+    }
+}
