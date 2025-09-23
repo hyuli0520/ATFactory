869c3e67bd70798384c96314d40bf4a109137295
hyuli0520 <minhyeogjeon945@gmail.com>
2025-09-23 18:07:04 +0900

    feat: Update Minable Item
    Create MineDigger

--- Assets/3.Scripts/Object/Rock.cs
+++ Assets/3.Scripts/Object/Rock.cs
@@ -5,6 +5,7 @@ public class Rock : MonoBehaviour, IMinable
     [SerializeField] private int durability = 5;
     public bool IsDepleted => durability <= 0;
     public ItemData itemData;
+    public ItemData Data => itemData;
 
     public void Mine(int power)
     {
@@ -18,6 +19,21 @@ public class Rock : MonoBehaviour, IMinable
         {
             Managers.UI.inven.AddItem(itemData);
             Destroy(gameObject);
+        }
+    }
+
+    public void MineDigger(int power, IMinable minable)
+    {
+        if (IsDepleted)
+            return;
+
+        durability -= power;
+        Debug.Log($"Rock mined, Remaining: {durability}");
+
+        if (IsDepleted)
+        {
+            Managers.UI.digger.Digging(minable.Data);
+            durability = 5;
         }
     }
 }
