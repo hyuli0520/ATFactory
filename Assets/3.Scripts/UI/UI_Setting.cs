189b8eeb0d91590bd2e727eabd7db2939c81e7ab
hyuli0520 <minhyeogjeon945@gmail.com>
2025-09-30 10:45:42 +0900

    feat: Create Language UI

--- Assets/3.Scripts/UI/UI_Setting.cs
+++ Assets/3.Scripts/UI/UI_Setting.cs
@@ -1,11 +1,43 @@
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 
 public class UI_Setting : MonoBehaviour
 {
+    public GameObject bindingKey;
+    public GameObject localization;
+
     public void Exit()
     {
         Managers.UI.setting.gameObject.SetActive(false);
         Time.timeScale = 1;
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void ClickBinding()
+    {
+        bindingKey.SetActive(true);
+        localization.SetActive(false);
+    }
+    public void ClickLocalization()
+    {
+        bindingKey.SetActive(false);
+        localization.SetActive(true);
+    }
+
+    public void ClickEnglish()
+    {
+        UpdateLocalization(0);
+    }
+    public void ClickKorea()
+    {
+        UpdateLocalization(2);
+    }
+    public void ClickJapanese()
+    {
+        UpdateLocalization(1);
+    }
+    public void UpdateLocalization(int index)
+    {   
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
     }
 }
